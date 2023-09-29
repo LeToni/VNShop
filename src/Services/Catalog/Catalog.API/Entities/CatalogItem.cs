1b8e89f149f3121d0c61f3dcf62c4c4639793351
Toni Le <t.le@andagon.com>
2023-09-29 15:49:32 +0200

    Initial empty projects

---
+++ src/Services/Catalog/Catalog.API/Entities/CatalogItem.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class CatalogItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("title")]
+        public string Name { get; set; }
+        public decimal price { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+        public CatalogRating rating { get; set; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Entities/CatalogRating.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Entities
+{
+    public class CatalogRating
+    {
+        public double rate { get; set; }
+        public long count { get; set; }
+    }
+}
