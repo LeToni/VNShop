6f4573f142650be5f586797161c6e78c6cfcdb29
Toni Le <toni.le@rwth-aachen.de>
2023-11-28 16:46:16 +0100

    Update project file to enable functional test

--- src/Services/Basket/Basket.API/Entities/BasketItem.cs
+++ src/Services/Basket/Basket.API/Entities/BasketItem.cs
@@ -3,7 +3,7 @@ namespace Basket.API.Entities;
 public class BasketItem
 {
     public string Id { get; set; }
-    public int Productid { get; set; }
+    public int ProductId { get; set; }
     public string ProductName { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
